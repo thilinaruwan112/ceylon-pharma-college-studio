64aec991aed165e84a121e41503b65d0a6dcd0cc
Thilina Ruwan <thilina20250527@gmail.com>
2025-07-08 11:15:26 +0000

    in course cards section in mobile screen show 1.3 cards

--- src/components/course-slider.tsx
+++ src/components/course-slider.tsx
@@ -64,7 +64,7 @@ export default function CourseSlider() {
         >
           <CarouselContent className="-ml-4">
             {courses.map((course, index) => (
-              <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3 xl:basis-1/5">
+              <CarouselItem key={index} className="pl-4 basis-3/4 md:basis-1/2 lg:basis-1/3 xl:basis-1/5">
                 <Link href="#" className="block h-full group">
                   <div className="p-1 h-full">
                     <Card className="overflow-hidden h-full flex flex-col transition-all duration-300 group-hover:shadow-xl group-hover:-translate-y-1">
